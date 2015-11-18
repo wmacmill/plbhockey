8306b5d25947d32e83c86d89f39307e6e7fb68a5
Will MacMillan <macmillan.will@gmail.com>
2015-11-18 09:38:40 -0500

    Updated 5 plugins
    Apppresser & a few others.

--- wp-content/plugins/wp-smush-pro/lib/class-wp-smush-admin.php
+++ wp-content/plugins/wp-smush-pro/lib/class-wp-smush-admin.php
@@ -1335,15 +1335,11 @@ if ( ! class_exists( 'WpSmushitAdmin' ) ) {
 		}
 
 		/**
-		 * Delete Site transient, stored for api status
+		 * Delete Site Option, stored for api status
 		 */
 		function refresh_status() {
-			global $WpSmush;
 
-			$api_key = $WpSmush->_get_api_key();
-			$key     = "wp-smush-premium-" . substr( $api_key, - 5, 5 );
-
-			delete_site_transient( $key );
+			delete_site_option('wp_smush_api_auth');
 		}
 	}
 
--- wp-content/plugins/wp-smush-pro/lib/class-wp-smush.php
+++ wp-content/plugins/wp-smush-pro/lib/class-wp-smush.php
@@ -48,8 +48,8 @@ if ( ! class_exists( 'WpSmush' ) ) {
 			//Auto Smush the new image
 			if ( $auto_smush ) {
 				add_filter( 'wp_update_attachment_metadata', array(
-					$this,
-					'filter_generate_attachment_metadata'
+						$this,
+						'filter_generate_attachment_metadata'
 				), 12, 2 );
 			}
 
@@ -209,11 +209,11 @@ if ( ! class_exists( 'WpSmush' ) ) {
 		 */
 		function _get_size_signature() {
 			return array(
-				'percent'     => - 1,
-				'bytes'       => - 1,
-				'size_before' => - 1,
-				'size_after'  => - 1,
-				'time'        => - 1
+					'percent'     => - 1,
+					'bytes'       => - 1,
+					'size_before' => - 1,
+					'size_after'  => - 1,
+					'time'        => - 1
 			);
 		}
 
@@ -237,12 +237,12 @@ if ( ! class_exists( 'WpSmush' ) ) {
 
 			$errors = new WP_Error();
 			$stats  = array(
-				"stats" => array_merge( $this->_get_size_signature(), array(
-						'api_version' => - 1,
-						'lossy'       => - 1
-					)
-				),
-				'sizes' => array()
+					"stats" => array_merge( $this->_get_size_signature(), array(
+									'api_version' => - 1,
+									'lossy'       => - 1
+							)
+					),
+					'sizes' => array()
 			);
 
 			$size_before = $size_after = $compression = $total_time = $bytes_saved = 0;
@@ -288,13 +288,13 @@ if ( ! class_exists( 'WpSmush' ) ) {
 					}
 					if( $ext ) {
 						$valid_mime = array_search(
-							$ext,
-							array(
-								'jpg' => 'image/jpeg',
-								'png' => 'image/png',
-								'gif' => 'image/gif',
-							),
-							true
+								$ext,
+								array(
+										'jpg' => 'image/jpeg',
+										'png' => 'image/png',
+										'gif' => 'image/gif',
+								),
+								true
 						);
 						if ( false === $valid_mime ) {
 							continue;
@@ -325,7 +325,7 @@ if ( ! class_exists( 'WpSmush' ) ) {
 					//Total Stats, store all data in bytes
 					if ( isset( $response['data'] ) ) {
 						list( $size_before, $size_after, $total_time, $compression, $bytes_saved )
-							= $this->_update_stats_data( $response['data'], $size_before, $size_after, $total_time, $bytes_saved );
+								= $this->_update_stats_data( $response['data'], $size_before, $size_after, $total_time, $bytes_saved );
 					} else {
 						$errors->add( "image_size_error" . $size_key, sprintf( __( "Size '%s' not processed correctly", 'wp-smushit' ), $size_key ) );
 					}
@@ -365,7 +365,7 @@ if ( ! class_exists( 'WpSmush' ) ) {
 				//Update stats
 				if ( isset( $full_image_response['data'] ) ) {
 					list( $size_before, $size_after, $total_time, $compression, $bytes_saved )
-						= $this->_update_stats_data( $full_image_response['data'], $size_before, $size_after, $total_time, $bytes_saved );
+							= $this->_update_stats_data( $full_image_response['data'], $size_before, $size_after, $total_time, $bytes_saved );
 				} else {
 					$errors->add( "image_size_error", __( "Size 'full' not processed correctly", 'wp-smushit' ) );
 				}
@@ -383,7 +383,7 @@ if ( ! class_exists( 'WpSmush' ) ) {
 			$has_errors = (bool) count( $errors->get_error_messages() );
 
 			list( $stats['stats']['size_before'], $stats['stats']['size_after'], $stats['stats']['time'], $stats['stats']['percent'], $stats['stats']['bytes'] ) =
-				array( $size_before, $size_after, $total_time, $compression, $bytes_saved );
+					array( $size_before, $size_after, $total_time, $compression, $bytes_saved );
 
 			//Set smush status for all the images, store it in wp-smpro-smush-data
 			if ( ! $has_errors ) {
@@ -461,8 +461,8 @@ if ( ! class_exists( 'WpSmush' ) ) {
 			$file      = @fopen( $file_path, 'r' );
 			$file_data = fread( $file, $file_size );
 			$headers   = array(
-				'accept'       => 'application/json', // The API returns JSON
-				'content-type' => 'application/binary', // Set content type to binary
+					'accept'       => 'application/json', // The API returns JSON
+					'content-type' => 'application/binary', // Set content type to binary
 			);
 
 			//Check if premium member, add API key
@@ -482,10 +482,10 @@ if ( ! class_exists( 'WpSmush' ) ) {
 
 			$api_url = defined( 'WP_SMUSH_API_HTTP' ) ? WP_SMUSH_API_HTTP : WP_SMUSH_API;
 			$args   = array(
-				'headers'    => $headers,
-				'body'       => $file_data,
-				'timeout'    => WP_SMUSH_TIMEOUT,
-				'user-agent' => WP_SMUSH_UA,
+					'headers'    => $headers,
+					'body'       => $file_data,
+					'timeout'    => WP_SMUSH_TIMEOUT,
+					'user-agent' => WP_SMUSH_UA,
 			);
 			$result = wp_remote_post( $api_url, $args );
 
@@ -598,41 +598,88 @@ if ( ! class_exists( 'WpSmush' ) ) {
 
 			//no api key set, always false
 			$api_key = $this->_get_api_key();
+
 			if ( empty( $api_key ) ) {
 				return false;
 			}
 
-			//W3TC disk cache is buggy and always converts 0 to false, so we have to store as string
-			$key = "wp-smush-premium-" . substr( $api_key, - 5, 5 ); //add last 5 chars of apikey to transient key in case it changes
-			$valid = get_site_transient( $key );
-			if ( empty( $valid ) ) {
+			//Flag to check if we need to revalidate the key
+			$revalidate = false;
+
+			$api_auth = get_site_option('wp_smush_api_auth');
+
+			//Check if need to revalidate
+			if ( ! $api_auth || empty( $api_auth ) || empty( $api_auth[ $api_key ] ) ) {
+				$revalidate = true;
+			} else {
+				$last_checked = $api_auth[ $api_key ]['timestamp'];
+				$valid        = $api_auth[ $api_key ]['validity'];
+
+				$diff = $last_checked - current_time( 'timestamp' );
+
+				//Difference in hours
+				$diff_h = $diff / 3600;
+
+				//Difference in minutes
+				$diff_m = $diff / 60;
+
+				switch ( $valid ) {
+					case 'valid':
+						//if last checked was more than 12 hours
+						if ( $diff_h > 12 ) {
+							$revalidate = true;
+						}
+						break;
+					case 'invalid':
+						//if last checked was more than 24 hours
+						if ( $diff_h > 24 ) {
+							$revalidate = true;
+						}
+						break;
+					case 'network_failure':
+						//if last checked was more than 5 minutes
+						if ( $diff_m > 5 ) {
+							$revalidate = true;
+						}
+						break;
+				}
+			}
+			//If we are suppose to validate api, update the results in options table
+			if ( $revalidate ) {
 				// call api
 				$url = $this->api_server . '&key=' . urlencode( $api_key );
 
 				$request = wp_remote_get( $url, array(
-						"user-agent" => WP_SMUSH_UA,
-						"timeout"    => 10
-					)
+								"user-agent" => WP_SMUSH_UA,
+								"timeout"    => 10
+						)
 				);
 
 				if ( ! is_wp_error( $request ) && '200' == wp_remote_retrieve_response_code( $request ) ) {
 					$result = json_decode( wp_remote_retrieve_body( $request ) );
 					if ( $result && $result->success ) {
-						$valid = 'true';
-						set_site_transient( $key, 'true', 12 * HOUR_IN_SECONDS );
+						$valid = 'valid';
 					} else {
-						$valid = 'false';
-						set_site_transient( $key, 'false', DAY_IN_SECONDS );
+						$valid = 'invalid';
 					}
 
 				} else {
-					$valid = 'false';
-					set_site_transient( $key, 'false', 5 * MINUTE_IN_SECONDS ); //cache network failure even shorter, we don't want a request every pageload
+					$valid = 'network_failure';
 				}
+
+				//Reset Value
+				$api_auth = array();
+
+				//Update Timestamp
+				$timestamp              = current_time( 'timestamp' );
+				$api_auth[ $api_key ] = array( 'validity' => $valid, 'timestamp' => $timestamp );
+
+				//Update API validity
+				update_site_option( 'wp_smush_api_auth', $api_auth );
 
 			}
 
-			$this->is_pro = ( 'true' == $valid );
+			$this->is_pro = ( 'valid' == $valid );
 
 			return $this->is_pro;
 		}
@@ -913,7 +960,7 @@ if ( ! class_exists( 'WpSmush' ) ) {
 				//Update total bytes saved, and compression percent
 				//Update Main Stats
 				list( $stats['stats']['size_before'], $stats['stats']['size_after'], $stats['stats']['time'], $stats['stats']['percent'], $stats['stats']['bytes'] ) =
-					$this->_update_stats_data( $data, $stats['stats']['size_before'], $stats['stats']['size_after'], $stats['stats']['time'], $stats['stats']['bytes'] );
+						$this->_update_stats_data( $data, $stats['stats']['size_before'], $stats['stats']['size_after'], $stats['stats']['time'], $stats['stats']['bytes'] );
 
 
 				//Update stats for each size
@@ -932,18 +979,18 @@ if ( ! class_exists( 'WpSmush' ) ) {
 			} else {
 				//Create new stats
 				$stats                         = array(
-					"stats" => array_merge( $this->_get_size_signature(), array(
-							'api_version' => - 1,
-							'lossy'       => - 1
-						)
-					),
-					'sizes' => array()
+						"stats" => array_merge( $this->_get_size_signature(), array(
+										'api_version' => - 1,
+										'lossy'       => - 1
+								)
+						),
+						'sizes' => array()
 				);
 				$stats['stats']['api_version'] = $data->api_version;
 				$stats['stats']['lossy']       = $data->lossy;
 				//Update Main Stats
 				list( $stats['stats']['size_before'], $stats['stats']['size_after'], $stats['stats']['time'], $stats['stats']['percent'], $stats['stats']['bytes'] ) =
-					array( $data->before_size, $data->after_size, $data->time, $data->compression, $data->bytes_saved );
+						array( $data->before_size, $data->after_size, $data->time, $data->compression, $data->bytes_saved );
 				//Update size wise details
 				$stats['sizes'][ $image_size ] = (object) $this->_array_fill_placeholders( $this->_get_size_signature(), (array) $data );
 			}
@@ -1018,21 +1065,21 @@ if ( ! class_exists( 'WpSmush' ) ) {
 					$file_size = file_exists( $full_image ) ? filesize( $full_image ) : '';
 					if ( !empty( $file_size ) && ( $file_size / WP_SMUSH_MAX_BYTES ) > 1 ) {
 						$skipped[] = array(
-							'size'   => 'full',
-							'reason' => 'size_limit'
+								'size'   => 'full',
+								'reason' => 'size_limit'
 						);
 					}else{
 						$skipped[] = array(
-							'size'   => 'full',
-							'reason' => 'large_size'
+								'size'   => 'full',
+								'reason' => 'large_size'
 						);
 					}
 				} else {
 					//Paid version, Check if we have large size
 					if ( array_key_exists( 'large', $size_stats ) ) {
 						$skipped[] = array(
-							'size'   => 'full',
-							'reason' => 'large_size'
+								'size'   => 'full',
+								'reason' => 'large_size'
 						);
 					}
 
@@ -1048,8 +1095,8 @@ if ( ! class_exists( 'WpSmush' ) ) {
 						$image_size = file_exists( $img_path ) ? filesize( $img_path ) : '';
 						if ( ! empty( $image_size ) && ( $image_size / WP_SMUSH_MAX_BYTES ) > 1 ) {
 							$skipped[] = array(
-								'size'   => 'full',
-								'reason' => 'size_limit'
+									'size'   => 'full',
+									'reason' => 'size_limit'
 							);
 						}
 					}
@@ -1066,10 +1113,10 @@ if ( ! class_exists( 'WpSmush' ) ) {
 		 */
 		function skip_reason( $msg_id ) {
 			$count = count( get_intermediate_image_sizes() );
-			$smush_orgnl_txt = sprintf( esc_html__("When you upload an image to WordPress it automatically creates %s thumbnail sizes that are commonly used in your pages. WordPress also stores the original full-size image, but because these are not usually embedded on your site we don’t Smush them. Pro users can override this.", 'wp_smushit'), $count );
+			$smush_orgnl_txt = sprintf( esc_html__("When you upload an image to WordPress it automatically creates %s thumbnail sizes that are commonly used in your pages. WordPress also stores the original full-size image, but because these are not usually embedded on your site we don’t Smush them. Pro users can override this.", 'wp_smushit'), $count );	     		   	  	  				
 			$skip_msg = array(
-				'large_size' => $smush_orgnl_txt,
-				'size_limit' => esc_html__( "Image couldn't be smushed as it exceeded the 1Mb size limit, Pro users can smush images with size upto 32Mb.", "wp-smushit" )
+					'large_size' => $smush_orgnl_txt,
+					'size_limit' => esc_html__( "Image couldn't be smushed as it exceeded the 1Mb size limit, Pro users can smush images with size upto 32Mb.", "wp-smushit" )
 			);
 			$skip_rsn = !empty( $skip_msg[$msg_id ] ) ? esc_html__(" Skipped", 'wp-smushit', 'wp-smushit'): '';
 			$skip_rsn = ! empty( $skip_rsn ) ? $skip_rsn . '<span class="dashicons dashicons-editor-help" title="' . $skip_msg[ $msg_id ] . '"></span>' : '';
@@ -1197,15 +1244,15 @@ if ( ! class_exists( 'WpSmush' ) ) {
 
 			if ( isset( $orderby ) && 'smushit' == $orderby ) {
 				$query->set( 'meta_query', array(
-					'relation' => 'OR',
-					array(
-						'key'     => $this->smushed_meta_key,
-						'compare' => 'EXISTS'
-					),
-					array(
-						'key'     => $this->smushed_meta_key,
-						'compare' => 'NOT EXISTS'
-					)
+						'relation' => 'OR',
+						array(
+								'key'     => $this->smushed_meta_key,
+								'compare' => 'EXISTS'
+						),
+						array(
+								'key'     => $this->smushed_meta_key,
+								'compare' => 'NOT EXISTS'
+						)
 				) );
 				$query->set( 'orderby', 'meta_value_num' );
 			}
